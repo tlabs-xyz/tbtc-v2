59e71efbdea3e7398bc56e57a275b3a000edab9c
Leonardo Saturnino <lrsaturnino@gmail.com>
2025-06-09 07:27:10 -0300

    feat(starknet): update StarkNetBitcoinDepositor and deployment artifacts
    - Added new tBTC bridging functionality to StarkNetBitcoinDepositor, including fee estimation and event emissions for bridging operations.
    - Removed unused variables and events related to L1→L2 message fees and fee buffers to streamline the contract.
    - Updated deployment artifacts for Sepolia with the latest contract address and transaction hash.
    - Enhanced deployment script for StarkNet with updated StarkGate bridge address for Sepolia.
    - Improved event logging for better tracking of bridging transactions.

--- cross-chain/starknet/contracts/hardhat-dependency-compiler/@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol
+++ cross-chain/starknet/contracts/hardhat-dependency-compiler/@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol
@@ -1,3 +1,3 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >0.0.0;
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol';
