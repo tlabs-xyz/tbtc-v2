27ce40add7236c4f14ae078694fe4da9f2ea18d7
evandrosaturnino <evandro.saturnino@gmail.com>
2025-07-20 22:01:15 -0300

    chore: update slither version for bob

--- cross-chain/bob/contracts/OptimismMintableUpgradableTBTC.sol
+++ cross-chain/bob/contracts/OptimismMintableUpgradableTBTC.sol
@@ -313,5 +313,6 @@ contract OptimismMintableUpgradableTBTC is
     /// @dev This empty reserved space is put in place to allow future versions to add new
     ///      variables without shifting down storage in the inheritance chain.
     ///      See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+    // slither-disable-next-line unused-state
     uint256[45] private __gap;
 }
