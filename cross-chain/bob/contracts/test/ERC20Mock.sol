b5f9bcc57633d59485716cf7c35a172ec8c85cff
evandrosaturnino <evandro.saturnino@gmail.com>
2025-08-28 12:45:43 -0300

    ERC20Mock.sol stays also in BOB folder

---
+++ cross-chain/bob/contracts/test/ERC20Mock.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Mock is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        address initialHolder,
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
+        _mint(initialHolder, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public {
+        _spendAllowance(account, _msgSender(), amount);
+        _burn(account, amount);
+    }
+}
